2f27ca7e6e617203bbaa5e17a7e5b840dba86954
robert1996 <rhanesoghlyan@gmail.com>
2017-04-17 19:09:55 +0500

    added config files to the config directory

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var server = require('./server');
-var router = express.Router();
+var config = require('./server/node_modules/common/config/env');
 
 var app = express();
 var admin = express();
 
-server.startApp(app, router);
-server.startAdmin(admin);
+app.get('/', function (request, response) {
+    response.send('<h1>Hello world</h1>');
+});
+
+app.listen(config.port, function () {
+    console.log('Express server listening on port', config.port);
+});
+
+app.use('/admin', admin);
+
+server.start(app, config.front);
+server.start(admin, config.admin);
+
